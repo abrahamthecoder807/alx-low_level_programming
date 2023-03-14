15e96b6f1f79f84c5e2b435f912162d96cee7fac
abrahamthecoder807 <abrahamj1807@gmail.com>
2023-03-14 15:14:26 -0700

    You'd better wake up

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,39 +3,29 @@
 #include "main.h"
 /**
  * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dim. array
+ * @width: An input integer at number of columns
+ * @height: An input integer at number of rows
+ * Return: pointer to 2D array, NULL on failure
  */
 int **alloc_grid(int width, int height)
 {
-	int **iarray;
-	int i, n;
+	int **array, i = 0, j = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	iarray = malloc(sizeof(int) * height);
-
-	if (iarray == NULL)
-	{
-		free(iarray);
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
 		return (NULL);
+	for (; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
-	{
-		iarray[i] = malloc(sizeof(int) * width);
-		if (iarray == NULL)
-		{
-			free(iarray);
-			return (NULL);
-		}
-
-		for (n = 0; n < width; n++)
-		{
-			iarray[i][n] = 0;
-		}
-	}
-	return (iarray);
+		for (; j < width; j++)
+			array[i][j] = 0;
+	return (array);
 }
