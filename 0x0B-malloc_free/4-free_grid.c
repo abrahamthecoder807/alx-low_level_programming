335baa973349daec85826d0e8fd472e50e50b600
abrahamthecoder807 <abrahamj1807@gmail.com>
2023-03-15 00:16:36 -0700

    Minimal number of coin for change

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - use free function on previous grid
+ * @grid: 2 dim. array to free
+ * @height: input height (size)
+ * Return: free'd grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 00; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
