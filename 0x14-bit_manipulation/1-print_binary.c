085dc1a22cd4b67593820b7a6d81a2b602a7a332
abrahamthecoder807 <abrahamj1807@gmail.com>
2023-05-04 18:21:55 -0700

    Second task on bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include  "main.h"
+/**
+ * print_binary - that prints the binary representation of a number
+ * @n: number
+ * Description: Write a function that prints
+ * Return:
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 2, j = 0;
+
+	while (i <= n && j < 62)
+	{
+		i = i << 1;
+		j++;
+	}
+
+	if (j != 62)
+		i = i >> 1;
+
+	while (i != 0)
+	{
+		if ((i & n) != 0)
+		{
+			j++;
+			putchar('1');
+		}
+		else
+		{
+			j++;
+			putchar('0');
+		}
+		i = i >> 1;
+	}
+}
